8e491ec5b45e0abee413de2b3c98013b29ad1e90
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-03-08 20:58:31 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){//begin
+
+    //numeric Luas, P, L 
+    double Luas, P, L;
+    //Display, "Masikan Panjangnya : "
+    cout << "Masukan Panjangnya : ";
+    //Accept P
+    cin >> P;
+    //Display "Masukan Lebarnya : "
+    cout << "Masukan Lebarnya : ";
+    //Accept L
+    cin >> L;
+    //Compute Luas = PL
+    Luas = P*L;
+    //Display, "Luasnya : " + Luas
+    cout << "Luasnya : "<< Luas;
+}//end
